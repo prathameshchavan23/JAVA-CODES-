0ab81ec7186a1f3ee9912526647d2a3f83e7e9f8
Prathamesh_chavan23 <105577462+prathameshchavan23@users.noreply.github.com>
2023-02-21 23:15:39 +0530

    Add files via upload

---
+++ addition_no.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class addition_no {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num1= input.nextInt();
+        int num2 = input.nextInt();
+
+        int sum=num1+num2;
+        System.out.println("Sum is = "+sum);
+          int num = (int) (34.8f); //--> Type-Casting
+          System.out.println(num);   
+
+        //   int a=256;
+        //   byte b= (byte)(a); // Here the compiler will take the quotient of 256 for this it will be 256 % 257=1
+    }
+}
