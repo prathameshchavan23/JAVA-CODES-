1c6e4988bf00eb959721e3adb833277f6408f7e7
Prathamesh_chavan23 <105577462+prathameshchavan23@users.noreply.github.com>
2023-03-09 18:19:12 +0530

    Add files via upload

---
+++ calc_area_of_triangle.java
@@ -0,0 +1,22 @@
+import java.lang.*;
+import java.util.Scanner;
+class calc_area_of_triangle {
+    public static void main(String[] args) {
+     Scanner sc=new Scanner(System.in);
+     int a,b,c;
+     float s;
+     double area;
+     System.out.println("Enter 3 sides of a triangle ");
+     a=sc.nextInt();
+     b=sc.nextInt();
+     c=sc.nextInt();
+
+     s=(a+b+c)/2f;
+     area=Math.sqrt(s*(s-a)*(s-b)*(s-c));
+     System.out.println("Area is "+area);
+
+    }
+        
+    
+}
+ 
